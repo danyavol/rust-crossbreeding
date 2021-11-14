60aafe2506c05c0882b6b5d3426f01c8eb6ff358
Volosyuk Daniil <danya.volosyuk@gmail.com>
2021-11-14 21:35:33 +0300

    feat: Deploy functionality

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
+# abort on errors
+set -e
+# build
 npm run build
-
 cd dist
-
-
 git init
 git add -A
 git commit -m 'deploy'
-
 git push -f git@github.com:danyavol/rust-crossbreeding.git master:gh-pages
-
-read -p "Press any key to resume ..."
+cd -
